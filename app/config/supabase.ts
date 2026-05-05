ea18bd949cf3a2f05d017f17d71808c758c2871c
PHENO47 <nzsamu338@gmail.com>
2026-05-05 23:08:56 +0100

    Sécurité : utilise variables d'environnement pour clés Supabase

--- app/config/supabase.ts
+++ app/config/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://fhhrijytqchdzanurqfj.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoaHJpanl0cWNoZHphbnVycWZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5OTY4NjYsImV4cCI6MjA5MzU3Mjg2Nn0.ysss7n-Opok6fRVHC1X7LGRKG9ZY90R72bE2MEajveI'
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
